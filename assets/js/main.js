f606f26044a46329fe811bf6110dd41f9a961de5
karfaisarra <karfaisarra@gmail.com>
2022-10-17 15:22:28 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+/* funzioni */   
+function cellsCreation(maxCells, containerEl){
+    for (let i = 1; i <= maxCells; i++) {
+            //nel ciclo for aggiungo le caselle dentro al container
+            const cellMarkup = `<div class="cella">${i}</div>`
+            containerEl.innerHTML += cellMarkup;
+    } 
+}
+function interazione(elencoCasella) {
+    for (let i = 0; i < elencoCasella.length; i++) {
+        const casellaAttuale = elencoCasella[i];
+        //console.log(casellaAttuale);
+        casellaAttuale.addEventListener('click', function(){
+            //console.log('ho cliccato sulla casella');
+            casellaAttuale.classList.toggle('azzurro');
+            console.log(this);
+        })
+    } 
+}
+/* ################################################## */ 
+
+let containerElement = document.querySelector('.container');
+containerElement += cellsCreation(100, containerElement);
+const cellsEl = document.querySelectorAll('.cella');
+interazione(cellsEl);
+const buttonEl = document.querySelector('.btn');
+
